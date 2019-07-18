e6c2fbf7ba9f445f4c98e07c70993eef563d7403
aaronshivers <aaron@moonmail.org>
2019-07-18 14:00:18 -0500

    more styling

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,17 +2,15 @@ import React from 'react'
 
 const Results = props => (
   <div className="content-container">
-    <div className="text-body">
-      {
-        props.userChoice && <p className="text-body">You chose { props.userChoice }.</p>
-      }
-      {
-        props.cpuChoice && <p className="text-body">CPU chose { props.cpuChoice }.</p>
-      }
-      {
-        props.getWinner(props.userChoice, props.cpuChoice) && <p className="text-body">{ props.getWinner(props.userChoice, props.cpuChoice) }</p>
-      }
-    </div>
+    {
+      props.userChoice && <p className="results-text">You chose { props.userChoice }.</p>
+    }
+    {
+      props.cpuChoice && <p className="results-text">CPU chose { props.cpuChoice }.</p>
+    }
+    {
+      props.getWinner(props.userChoice, props.cpuChoice) && <p className="results-text">{ props.getWinner(props.userChoice, props.cpuChoice) }</p>
+    }
   </div>
 )
 
