5c05f1a1d9d888df9b13cb3ee415076cfe5b5fdb
aaronshivers <aaron@moonmail.org>
2019-07-18 14:36:31 -0500

    adjusted button and container sizes

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Results = props => (
   <div className="content-container">
     {
+      props.getWinner(props.userChoice, props.cpuChoice) && <p className="results-text">{ props.getWinner(props.userChoice, props.cpuChoice) }</p>
+    }
+    {
       props.userChoice && <p className="results-text">You chose { props.userChoice }.</p>
     }
     {
       props.cpuChoice && <p className="results-text">CPU chose { props.cpuChoice }.</p>
-    }
-    {
-      props.getWinner(props.userChoice, props.cpuChoice) && <p className="results-text">{ props.getWinner(props.userChoice, props.cpuChoice) }</p>
     }
   </div>
 )
